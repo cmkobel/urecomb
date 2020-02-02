0057721bbc133bd3d2db8f94a9b9b4ff2d57f454
Carl M. Kobel <kobel@protonmail.ch>
2020-02-02 01:40:53 +0100

    added lots of features. Most should be working OK. Trying to get done extracting genes and measuring GC

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -37,10 +37,10 @@ target_dir = '/faststorage/project/ClinicalMicrobio/10carl/urecomb/workflow/outp
 reference_genomes = {'S_mitis_tg': 'mitis_b6'}
                       #'Aactinomycetemcomitans': 'path'}
 for group, reference_stem in reference_genomes.items(): # index all genomes
-	print(group, reference_stem)
+    print(group, reference_stem)
 
-	gwf.target_from_template(sanify('ure_smaltindex_' + group),
-		                     smalt_index(target_dir, group, reference_stem))
+    gwf.target_from_template(sanify('ure_smaltindex_' + group),
+                             smalt_index(target_dir, group, reference_stem))
 
 
 
@@ -51,22 +51,32 @@ print(input_data.columns)
 
 for sample_, sample in input_data.iterrows():
 
-	group = sample['group'].strip()
-	sample_name = sample['sample'].strip()
-	reference_stem = reference_genomes[group].strip()
+    group = sample['group'].strip()
+    sample_name = sample['sample'].strip()
+    reference_stem = reference_genomes[group].strip()
 
-	print(group, sample_name, reference_stem)
+    print(group, sample_name, reference_stem)
 
-	gwf.target_from_template(sanify('ure_smaltmap_' + group[0:5] + '_' + sample_name),
-		                     smalt_map(group, sample_name, sample['forward'].strip(), sample['reverse'].strip(), reference_stem))
+    # Map reads for each isolate to the reference
+    gwf.target_from_template(sanify('ure_smaltmap_' + group[0:5] + '_' + sample_name),
+                             smalt_map(group, sample_name, sample['forward'].strip(), sample['reverse'].strip(), reference_stem))
 
-	gwf.target_from_template(sanify('ure_mcorr_bf_' + group[0:5] + '_' + sample_name),
-		                     mcorr_bam_fit(group, sample_name, sample['forward'].strip(), sample['reverse'].strip(), reference_stem))
+    # infer and correlate recombination
+    gwf.target_from_template(sanify('ure_mcorr_' + group[0:5] + '_' + sample_name),
+                             mcorr_bam_fit(group, sample_name, sample['forward'].strip(), sample['reverse'].strip(), reference_stem))
 
+    # 
+    gwf.target_from_template(sanify('ure_consensus_' + group[0:5] + '_' + sample_name),
+                             consensus(group, sample_name, reference_stem))
+    
 
-	
+    gwf.target_from_template(sanify('ure_extract_' + group[0:5] + '_' + sample_name),
+                             extract_fasta_from_bed(group, sample_name, reference_stem))
+    
 
-	
+    
+    
+    break
 
 
 
--- workflow/workflow_templates.py
+++ workflow/workflow_templates.py
@@ -8,11 +8,13 @@ goodbye = "echo JOBID $SLURM_JOBID; jobinfo $SLURM_JOBID"
 
 def smalt_index(target_dir, group, reference_stem):
 
-	inputs = 'reference_genomes/' + reference_stem + '.fasta'
-	outputs = ['reference_genomes/' + reference_stem + '.smi',
-	           'reference_genomes/' + reference_stem + '.sma']
-	options = {'nodes': 1, 'cores': 1, 'memory': '8g', 'walltime': '1:00:00',  'account': 'clinicalmicrobio'}
-	spec = f"""
+    inputs = 'reference_genomes/' + reference_stem + '.fasta'
+    outputs = ['reference_genomes/' + reference_stem + '.smi',
+               'reference_genomes/' + reference_stem + '.sma',
+               'reference_genomes/' + reference_stem + '.gene.txt']
+    options = {'nodes': 1, 'cores': 1, 'memory': '8g', 'walltime': '1:00:00',  'account': 'clinicalmicrobio'}
+    
+    spec = f"""
 
 # mkdir {target_dir}/group
 # cd {target_dir}/group
@@ -21,53 +23,108 @@ cd reference_genomes/
 
 smalt index -k 14 -s 8 {reference_stem} {reference_stem}.fasta
 
+# Later, when we are going to extract the gene sequences from each isolate, we are only interested in the genes.
+cat {reference_stem}.gff3 | awk '$3 == "gene" {{print}}' > {reference_stem}.gene.txt
+
+
+
 
 {goodbye}
-	"""
-	return inputs, outputs, options, spec
+    """
+    return inputs, outputs, options, spec
 
 
 
 def smalt_map(group, sample, forward, reverse, reference_stem):
-	inputs = ['reference_genomes/' + reference_stem + '.smi',
-	           'reference_genomes/' + reference_stem + '.sma']
-	outputs = ['output/' + group + '/' + sample + '.sorted.bam']
-	options = {'nodes': 1, 'cores': 4, 'memory': '16g', 'walltime': '2:00:00',  'account': 'clinicalmicrobio'}
-	spec = f"""
+    inputs = ['reference_genomes/' + reference_stem + '.smi',
+               'reference_genomes/' + reference_stem + '.sma']
+    outputs = ['output/' + group + '/' + sample + '.sorted.bam']
+    options = {'nodes': 1, 'cores': 4, 'memory': '16g', 'walltime': '2:00:00',  'account': 'clinicalmicrobio'}
+    spec = f"""
 
 mkdir -p output/{group}
 cd output/{group}
 
 
 smalt map -n 4 ../../reference_genomes/{reference_stem} ../../{forward} ../../{reverse} | \
-samtools sort -t tmp > {sample}.sorted.bam
+samtools sort > {sample}.sorted.bam
+
 
 
 {goodbye}
-	"""
-	return inputs, outputs, options, spec
+    """
+    return inputs, outputs, options, spec
+
+
+
+
+def consensus(group, sample, reference_stem):
+    inputs = ['output/' + group + '/' + sample + '.sorted.bam']
+    outputs = ['output/' + group + '/' + sample + '.fasta']
+    options = {'nodes': 1, 'cores': 4, 'memory': '16g', 'walltime': '2:00:00',  'account': 'clinicalmicrobio'}
+    spec = f"""
+
+mkdir -p output/{group}
+cd output/{group}
+
+
+samtools mpileup -f ../../reference_genomes/{reference_stem}.fasta {sample}.sorted.bam | \
+GenomicConsensus --fna_file ../../reference_genomes/{reference_stem}.fasta > {sample}.fasta 
+
+
+{goodbye}
+    """
+    return inputs, outputs, options, spec
 
 
 
 def mcorr_bam_fit(group, sample, forward, reverse, reference_stem):
-	inputs = ['reference_genomes/' + reference_stem + '.gff3',
-	          'output/' + group + '/' + sample + '.sorted.bam']
-	outputs = ['output/' + group + '/' + sample + '.csv',  # mcorr_bam
-	           'output/' + group + '/' + sample + '.json', # mcorr_bam
-	           'output/' + group + '/' + sample + '_best_fit.svg',  # mcorr_fit
-	           'output/' + group + '/' + sample + '_fit_reports.txt',  # mcorr_fit
-	           'output/' + group + '/' + sample + '_fit_results.csv']  # mcorr_fit
-	           #'output/' + group + '/' + sample + '_parameter_histograms.svg']  # mcorr_fit
-	options = {'nodes': 1, 'cores': 4, 'memory': '24g', 'walltime': '2:00:00',  'account': 'clinicalmicrobio'}
-	spec = f"""
+    inputs = ['reference_genomes/' + reference_stem + '.gff3',
+              'output/' + group + '/' + sample + '.sorted.bam']
+    outputs = ['output/' + group + '/' + sample + '.csv',  # mcorr_bam
+               'output/' + group + '/' + sample + '.json', # mcorr_bam
+               'output/' + group + '/' + sample + '_best_fit.svg',  # mcorr_fit
+               'output/' + group + '/' + sample + '_fit_reports.txt',  # mcorr_fit
+               'output/' + group + '/' + sample + '_fit_results.csv']  # mcorr_fit
+               #'output/' + group + '/' + sample + '_parameter_histograms.svg']  # mcorr_fit # fejl muligvis relateret til -t -T i samtools sort
+    options = {'nodes': 1, 'cores': 4, 'memory': '64g', 'walltime': '2:00:00',  'account': 'clinicalmicrobio'}
+    spec = f"""
 mkdir -p output/{group}
 cd output/{group}
 
 mcorr-bam ../../reference_genomes/{reference_stem}.gff3 {sample}.sorted.bam {sample}
 
-mcorr-fit {sample}.csv {sample}
+mcorr-fit {sample}.csv {sample} || echo "probably not enough memory"
+
 
 
 {goodbye}
 """
-	return inputs, outputs, options, spec
+    return inputs, outputs, options, spec
+
+
+
+
+def extract_fasta_from_bed(group, sample, reference_stem):
+    inputs = [#'output/' + group + '/' + sample + '.sorted.bam',
+              'output/' + group + '/' + sample + '.fasta',
+              'reference_genomes/' + reference_stem + '.gff3']
+    outputs = ['output/' + group + '/' + sample + '.extracted.fasta']
+               #'output/' + group + '/' + sample + '_parameter_histograms.svg']  # mcorr_fit # fejl muligvis relateret til -t -T i samtools sort
+    options = {'nodes': 1, 'cores': 4, 'memory': '8g', 'walltime': '2:00:00',  'account': 'clinicalmicrobio'}
+    spec = f"""
+mkdir -p output/{group}
+cd output/{group}
+
+# The index is automatically generated
+bedtools getfasta -fi {sample}.fasta -bed ../../reference_genomes/{reference_stem}.gene.gff3 > {sample}.extracted.fasta
+
+
+
+
+{goodbye}
+"""
+    return inputs, outputs, options, spec
+
+
+
